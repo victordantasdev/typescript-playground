0856f661309a80776194602d50cfa0520c7b9b17
victordantasdev <victordantasdeveloper@gmail.com>
2023-01-13 20:04:51 -0300

    feat: adds server configuration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
+import express from 'express';
 import { sum } from "./utils/sum";
 
-const res = sum(35, 7);
-console.log({ res });
+const PORT = Number(process.argv?.[2]) || 4000;
+const app = express();
+
+app.listen(PORT, () => { console.log(`server running on http://localhost:${PORT}`) });
+
+app.get('/', (req, res) => {
+  const { query } = req;
+  const a = Number(query?.a);
+  const b = Number(query?.b);
+
+  if (Object.keys(query || {}).length) {
+    res.send({ result: sum(a, b) });
+    return;
+  }
+
+  res.send({
+    message: 'You can pass numbers as query params to be summed',
+    example: '/?a=35&b=7'
+  })
+})
