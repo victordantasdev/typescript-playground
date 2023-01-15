491a924401af6a3dae33f10b377f51ac4330a01e
victordantasdev <victordantasdeveloper@gmail.com>
2023-01-15 19:05:42 -0300

    fix: typo on sum test

--- src/__tests__/utils/sum.test.ts
+++ src/__tests__/utils/sum.test.ts
@@ -1,7 +1,7 @@
 import { sum } from "../../utils/sum"
 
 describe('sum', () => {
-  it('shoud sum two numbers', () => {
+  it('should sum two numbers', () => {
     expect(sum(35, 7)).toBe(42);
   });
 })
